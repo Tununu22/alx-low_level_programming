4928895327ad32f485cc1dea55361fdd14bbff2f
Tununu22 <mchllcherotich@gmail.com>
2023-07-18 14:14:19 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,6 @@
 #include "main.h"
+
+
 /**
  * main - Entry point 
  * Return: Always 0
@@ -6,14 +8,14 @@
 
 int main(void)
 {
-	 _putchar('_');
-	 _putchar('p');
-	 _putchar('u');
-	 _putchar('t');
-	 _putchar('c');
-	 _putchar('h');
-	 _putchar('a');
-	 _putchar('r');
-	 _putchar('\n');
-	 return(0);
+ _putchar('_');
+ _putchar('p');
+ _putchar('u');
+ _putchar('t');
+ _putchar('c');
+ _putchar('h');
+ _putchar('a');
+ _putchar('r');
+ _putchar('\n');
+ return(0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,14 +1,17 @@
+#ifndef MAIN_HEADER_FILE 
+#define MAIN_HEADER_FILE
+
 int _putchar(char c);
-void print alphabet(void);
-void print_alphabetx10(void);
+void print_alphabet(void);
+void print_alphabet_x10(void);
 int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
+int _isalpha(int n);
 int _abs(int);
 int print_last_digit(int);
-int jack_baver(void);
-void tines_table(void);
+void jack_bauer(void);
+void times_table(void);
 int add(int, int);
-void print_to_98(int n);
-void print_tines_table(int n);
+
+#endif 
+
 
