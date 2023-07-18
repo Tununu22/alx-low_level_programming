aa76734904af99b2b72188e5ceb4dbfb98bbd6f4
Tununu22 <mchllcherotich@gmail.com>
2023-07-18 06:55:41 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,14 @@
+int _putchar(char c);
+void print alphabet(void);
+void print_alphabetx10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+int jack_baver(void);
+void tines_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_tines_table(int n);
+
