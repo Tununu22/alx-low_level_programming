d27c0fcfb12e5c2af6bbc66d87bd0e84792de292
Tununu22 <mchllcherotich@gmail.com>
2023-07-24 15:11:49 -0700

    nothing

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
+
+
+/**
+ * void prototypes()
+ * char prototypes()
+ * int prototypes()
+ */
 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -14,7 +22,7 @@ char *_strcpy(char *dest, char *src);
 int _putchar(char c);
 int _atoi(char *s);
 
-#endif 
+#endif /*MAIN_H*/
 
 
 
