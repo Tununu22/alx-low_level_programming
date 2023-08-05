ed2e1ee90ffc0388db76326b2d5981e064cf4fd6
Tununu22 <mchllcherotich@gmail.com>
2023-08-04 19:06:04 -0700

    Without libraries what have we? We have no past and no future

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-gcc -Wall -Wextra -Werror -pendatic -c *.c 
-ar rc liball.a *.0
+
+# Compile all .c files into .o files
+gcc -c *.c
+
+# Create the static library
+ar rcs liball.a *.o
+
+# Clean up the .o files
+rm *.o
+
+
