950885cf91daac77eeaabdc630f6216a66d5c5f2
Tununu22 <mchllcherotich@gmail.com>
2023-08-04 18:02:56 -0700

    libmy.a

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -4,19 +4,8 @@
 #include <ctype.h>
 #include <string.h>
 
-/**
- *_puts - prints a string
- * @str: string to print
- *
- * Description: prints a string
- * On success: return the number of characters printed
- */
-
-void _puts(char *str)
+void _puts(char *s)
 {
-while (*str)
-_putchar(*str++);
-
-_putchar('\n');
+    puts(s);
 }
 
